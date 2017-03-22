b594d83b233cff9463c2220dca9e9aadfd8bba92
vachon <nicolas.vachon.97@gmail.com>
2017-03-22 10:38:59 +0100

    add message frament

---
+++ channelmessaging/app/src/main/java/com/example/vachonn/channelmessaging/Fragment/MessageFragment.java
@@ -0,0 +1,22 @@
+package com.example.vachonn.channelmessaging.Fragment;
+
+import android.app.Fragment;
+import android.view.View;
+
+import com.example.vachonn.channelmessaging.OnDownloadCompleteListener;
+
+/**
+ * Created by vachonn on 22/03/2017.
+ */
+public class MessageFragment extends Fragment implements OnDownloadCompleteListener, View.OnClickListener, UploadFileToServer.OnUploadFileListener{
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onDownloadComplete(String result, int requestCode) {
+
+    }
+}
